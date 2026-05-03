e061450fac4e73b10ab2edca5691031274eb07de
eliapinmor <eliapinmor@monlau.com>
2026-05-04 00:39:09 +0200

    login in progress

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login implements OnInit {
+  form: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: Auth,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    this.auth.login(this.form.getRawValue()).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (err) => console.error(err),
+    });
+  }
+}
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +8,51 @@ import { Injectable } from '@angular/core';
 export class Auth {
   private apiUrl = 'http://localhost:3000/api/auth';
 
+private userSubject = new BehaviorSubject<any>(null);
+  user$ = this.userSubject.asObservable();
 
+  constructor(private http: HttpClient) {
+    const token = this.getToken();
+    if (token) {
+      this.getProfile().subscribe();
+    }
+  }
+
+  register(data: { name: string; email: string; password: string }) {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  login(data: { email: string; password: string }) {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, data)
+      .pipe(
+        tap(res => {
+          localStorage.setItem('token', res.token);
+          this.getProfile().subscribe();
+        })
+      );
+  }
+
+  getProfile() {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.getToken()}`
+    });
+
+    return this.http.get(`${this.apiUrl}/profile`, { headers })
+      .pipe(
+        tap(user => this.userSubject.next(user))
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.userSubject.next(null);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn() {
+    return !!this.getToken();
+  }
 }
